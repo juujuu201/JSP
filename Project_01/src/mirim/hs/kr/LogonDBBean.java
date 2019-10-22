7ded8729a90446ce35d65d79f5f41d234d1775cf
juhyun110701 <s2017s16@e-mirim.hs.kr>
2019-10-22 15:39:54 +0900

    1022

--- Project_01/src/mirim/hs/kr/LogonDBBean.java
+++ Project_01/src/mirim/hs/kr/LogonDBBean.java
@@ -1,6 +1,8 @@
 package mirim.hs.kr;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -24,5 +26,72 @@ public class LogonDBBean {
 		return ds.getConnection();
 	}//getConnection()
 	
+	public void insertMember(LogonDataBean member) throws Exception{
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		
+		try {
+			conn=getConnection();	//연결
+			
+			String sql="insert into member values(?,?,?,?,?,?,?,?)";
+			
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, member.getId());
+			pstmt.setString(2, member.getPasswd());
+			pstmt.setString(3, member.getName());
+			pstmt.setString(4, member.getJumin1());
+			pstmt.setString(5, member.getJumin2());
+			pstmt.setString(6, member.getEmail());
+			pstmt.setString(7, member.getBlog());
+			pstmt.setTimestamp(8, member.getReg_date());
+			
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally{
+			if(pstmt!=null){try{pstmt.close();}catch(Exception e){e.printStackTrace();}}
+			if(conn!=null){try{conn.close();}catch(Exception e){e.printStackTrace();}}
+		}
+	}//insertMember()
 	
+	public int userCheck(String id, String passwd) throws Exception{
+		Connection  conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String dbpasswd="";
+		int x=-1;
+		
+		try {
+			conn=getConnection();
+			
+			String sql="select passwd from member where id=?";
+			
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dbpasswd=rs.getString("passwd");
+				if(passwd.equals(dbpasswd)) {
+					x=1;
+				}
+				else {
+					x=0;
+				}
+			}
+			else {
+				x=-1;
+			}
+		}catch(Exception e) {
+			
+		}finally{
+			if(rs!=null){try{rs.close();}catch(Exception e){e.printStackTrace();}}
+			if(pstmt!=null){try{pstmt.close();}catch(Exception e){e.printStackTrace();}}
+			if(conn!=null){try{conn.close();}catch(Exception e){e.printStackTrace();}}
+		}
+		
+		return x;
+	}//userCheck(String id, String passwd)
 }
